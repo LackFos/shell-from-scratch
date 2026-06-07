a55034035a921dd7e62a86a7ac522953c3ae1f9a
LackFos <LackFosisOnline@gmail.com>
2026-06-07 20:11:36 +0700

    feat: add barrel file for command imports

---
+++ app/commands/index.ts
@@ -0,0 +1,8 @@
+export { cdCommand } from "./cd";
+export { echoCommand } from "./echo";
+export { exitCommand } from "./exit";
+export { helloCommand } from "./hello";
+export { pwdCommand } from "./pwd";
+export { typeCommand } from "./type";
+
+export type { ShellCommand } from "./types";
---
+++ app/commands/types.d.ts
@@ -0,0 +1,1 @@
+export type ShellCommand = (input: string) => void;
